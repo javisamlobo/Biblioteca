2982eb45a6a8ba0f3cdd62a5dd0dd60f1b2be7b2
javisamlobo <115726044+javisamlobo@users.noreply.github.com>
2022-11-03 17:06:39 +0100

    Genero

---
+++ src/com/Javier/Biblioteca/Genero.java
@@ -0,0 +1,15 @@
+package com.Javier.Biblioteca;
+
+public class Genero {
+    //Atributos
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    //Metodos
+    public Integer getId() { return id;}
+    public void setId(Integer id) { this.id = id;}
+    public String getNombre() { return nombre;}
+    public void setNombre(String nombre) { this.nombre = nombre;}
+    public String getDescripcion() { return descripcion;}
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion;}
+}
