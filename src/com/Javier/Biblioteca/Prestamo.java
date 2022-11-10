355280a0ea7d3176ff4784efe46809ff1184f2be
javisamlobo <115726044+javisamlobo@users.noreply.github.com>
2022-11-10 15:27:45 +0100

    Prestamo

--- src/com/Javier/Biblioteca/Prestamo.java
+++ src/com/Javier/Biblioteca/Prestamo.java
@@ -5,14 +5,11 @@ import java.util.Date;
 public class Prestamo {
     //Atributos
     private Integer id;
-    private  Libro.isbn libro;
-    private String usuario;
+    private Libro isbn;
+    private Usuario dni;
     private Date fecha_pres;
     private Date fecha_devo;
 
     //Metodos
-    public Integer getId() { return id;}
-    public void setId(Integer id) { this.id = id;}
-    public String getLibro() { return libro;}
-    public void setNombre(String nombre) { this.nombre = nombre;}
+
 }
